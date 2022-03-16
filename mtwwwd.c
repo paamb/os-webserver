936c06c071de9f761e2878e89700a94f3ec01b2a
Paal Markus Bjørnstad <paamb@stud.ntnu.no>
2022-03-16 12:48:18 +0100

    Filen leses, men får segmentation fault.

--- mtwwwd.c
+++ mtwwwd.c
@@ -6,17 +6,46 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #define PORT 8000
 #define MAXREQ (4096*1024)
 
 // Oppretter max lengde paa buffrene
-char buffer[MAXREQ], body[MAXREQ], msg[MAXREQ];
+char buffer[MAXREQ], body[MAXREQ], msg[MAXREQ], fpbuffer[MAXREQ];
 void error(const char *msg) { perror(msg); exit(1); }
 
-int main() {
-    char *path;
-    int port;
+void open_file_from_path(char *absolutePath, char dirPath[], char webpagePath[]){
+    absolutePath = malloc(strlen(dirPath)+strlen(webpagePath)+1);
+    strcpy(absolutePath, dirPath);
+    strcat(absolutePath, webpagePath);
+    printf("%s", absolutePath);
+    printf("%p", &absolutePath);
     
+    // FILE *fp;
+    // fp = fopen(absolutePath, "r");
+    printf("Ikke feil HER \n\n\n");
+    // return absolutePath;
+}
+
+void func(){  
+    char cwd[200];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current working dir: %s\n", cwd);
+    } else {
+        perror("getcwd() error");
+    }
+}
+
+int main() {
+    char dirPath[60];
+    int port;
+    FILE *fp;
+    
+    // Finds current directory
+    func();
+    printf("Skriv inn path ");
+    scanf("%59s", dirPath);
     
     //Sockfd - Socketen som blir opprettet
     int sockfd, connectsockfd;
@@ -57,6 +86,9 @@ int main() {
     listen(sockfd, 1);
 
     while(1){
+        // Finds current directory
+        func();
+        printf("hei");
         bzero(buffer,sizeof(buffer));
 
         clilen = sizeof(cli_addr);
@@ -66,17 +98,42 @@ int main() {
         if (connectsockfd < 0) error("ERROR on accept");
         n = read (connectsockfd,buffer,sizeof(buffer)-1);
 
-        int *phc;
+        // printf("%s", buffer);
+        // char *test;
+        // test = strtok(buffer, "/");
+        char *phc;
+        
         phc = strtok(buffer," "); // Cycling through GET
         phc = strtok(NULL," "); // phc is now the given URL
+        // char webpagePath[30];
+        printf("Ikke feil forst i main");
 
+        // const peker direkte til stringen
+        char *absolutePath;
+        absolutePath = malloc(strlen(dirPath)+strlen(phc)+1);
+        strcpy(absolutePath, dirPath);
+        strcat(absolutePath, phc);
+        printf("%s", absolutePath);
+        // absolutePath = open_file_from_path(absolutePath, dirPath, phc);
+        // open_file_from_path(absolutePath, dirPath, phc);
+        printf("andre gang %s", absolutePath);
+        FILE *fp;
+
+        if ((fp = fopen(absolutePath, "r")) == NULL){
+            printf("Fil kunne ikke aapnes");
+        }
+        printf("Den kan faktisk leses");
+        // printf("Mest sannsynlig her");
+        fgets(fpbuffer, sizeof(fpbuffer)-1, fp);
+        // printf("\n\nfds");
+        // printf("\n\n\n%s", test);
         snprintf (body, sizeof (body),
             "<html>\n<body>\n"
             "<h1>Hello web browser</h1>\nYour request was\n"
             "<pre>%s</pre>\n\n\n"
             "Your URL was\n"
             "<pre>%s</pre>"
-            "</body>\n</html>\n", buffer, phc);
+            "</body>\n</html>\n", phc, fpbuffer);
         
         snprintf (msg, sizeof (msg),
             "HTTP/1.0 200 OK\n"
